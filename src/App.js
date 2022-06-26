534a9229c85daa20c9522b8345063e735a9d7911
Afsana Meem <afsumeem@gmail.com>
2022-06-26 20:46:49 +0600

    icon added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from "./components/Home/Home";
 function App() {
   return (
     <div>
+      {/* home page*/}
       <Home/>
     </div>
   );
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -6,6 +6,7 @@ import image3 from "../../images/download (2).jpg"
 import Classes from '../Classes/Classes/Classes';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHeart, faShareSquare } from '@fortawesome/free-regular-svg-icons';
+import { faChevronRight} from '@fortawesome/free-solid-svg-icons';
 import "./Home.css";
 
 const Home = () => {
@@ -13,6 +14,7 @@ const Home = () => {
     // font awesome icon
     const heart = <FontAwesomeIcon icon={faHeart} />
     const share = <FontAwesomeIcon icon={faShareSquare} />
+    const arrow =<FontAwesomeIcon icon={faChevronRight}/>
 
     return (
         <div className="my-10 mx-10">
@@ -29,7 +31,7 @@ const Home = () => {
             {/* button */}
 
             <div className='items-center justify-between flex'>
-                <button type='button' className='bg-violet-700 text-slate-100 px-5 mt-6 py-2 rounded-full cursor-pointer hover:bg-violet-900'>See Class Schedule <FontAwesomeIcon icon="fa-regular fa-angle-right" /></button>
+                <button type='button' className='bg-violet-700 text-slate-100 px-5 mt-6 py-2 rounded-full cursor-pointer hover:bg-violet-900'>See Class Schedule {arrow}</button>
 
                 <p className='pt-6 text-violet-700 cursor-pointer hover:text-violet-900'>{heart} Save</p>
                 <p className='pt-6 text-violet-700 pr-14 cursor-pointer hover:text-violet-900'>{share} Share</p>
