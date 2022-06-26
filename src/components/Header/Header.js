6328c4e40124be33de271dfb15b97e8993a1b656
Afsana Meem <afsumeem@gmail.com>
2022-06-26 19:02:29 +0600

    image style updated

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,7 +8,7 @@ const Header = () => {
         <div>
             <h2 className='font-extrabold text-3xl'>Summer Art Camp! 5 Days of Artists and Painting Monet, <br/>Van Gogh, Matisse, & More</h2>
 
-            <p className='text-base font-semibold text-gray-500'>Multi-Day Course {questionMark}</p>
+            <p className='pt-2 text-base font-semibold text-gray-500'>Multi-Day Course {questionMark}</p>
         </div>
     );
 };
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,33 +4,39 @@ import image1 from "../../images/12085871_dsc-4005.jpg"
 import image2 from "../../images/download (1).jpg"
 import image3 from "../../images/download (2).jpg"
 import Classes from '../Classes/Classes/Classes';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHeart, faShareSquare } from '@fortawesome/free-regular-svg-icons';
 
 const Home = () => {
-    return (
-        <div className="h-96 my-10 mx-10">
-      <Header/>
-        <div className='grid grid-cols-1 md:grid-cols-2 pt-10'>
-           <div className=''>
-            <Classes/>
-            <div>
-            <button>See Class Schedule </button>
+    
+    const heart = <FontAwesomeIcon icon={faHeart} />
+    const share = <FontAwesomeIcon icon={faShareSquare} />
 
+    return (
+        <div className="my-10 mx-10">
+            <Header/>
+        <div className='grid grid-cols-1 md:grid-cols-2 pt-10'>
+           <div>
+            <Classes/>
+            <div className='items-center justify-between flex'>
+                <button type='button' className='bg-violet-700 text-slate-100 px-5 mt-6 py-2 rounded-full cursor-pointer hover:bg-violet-900'>See Class Schedule </button>
+
+                <p className='pt-6 text-violet-700 cursor-pointer hover:text-violet-900'>{heart} Save</p>
+
+                <p className='pt-6 text-violet-700 pr-14 cursor-pointer hover:text-violet-900'>{share} Share</p>
             </div>
            </div>
-
-            <div className=''>
-                <div className='grid grid-cols-1 md:grid-cols-2'>
+                <div className='grid grid-cols-1 md:grid-cols-2' >
                     <div>
-                      
-                    <img className='h-40' src={image1} alt='' />
-                           
+                        <img className='h-96' src={image1} alt='' /> 
                     </div>
+
                     <div>
-                    <img className='h-20' src={image2}  alt=''/>
-                    <img className='h-20'  src={image3} alt=''/>
+                        <img className='h-48' src={image2}  alt=''/>
+                        <img className='h-48 pt-6'  src={image3} alt=''/>
                     </div>
                 </div>
-            </div> 
+           
         </div>
         </div>
     );
